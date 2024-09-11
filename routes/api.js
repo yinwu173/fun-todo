65582f9b3ecb71be35f10aa1d387553943143d23
yinwu173 <yin.wu173@gmail.com>
2024-09-10 22:08:46 -0700

    Edited api.js: added psuedo code and exported api routes

---
+++ routes/api.js
@@ -0,0 +1,21 @@
+// Import package
+const apiRoutes = require('express').Router();
+
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+
+
+
+// POST /api/notes should receive a new note to save on the request body
+
+
+
+// Add it to the db.json file
+
+
+
+// Then return the new note to the client. 
+
+
+
+// Export apiRoutes
+module.exports = apiRoutes;
