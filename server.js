86a637cf3b86bdabe80f2f03dee2d031a232f786
yinwu173 <yin.wu173@gmail.com>
2024-09-10 21:06:02 -0700

    Edited server.js: initialized app, added middleware, and included listen method() on PORT 3001

--- server.js
+++ server.js
@@ -12,3 +12,20 @@ const apiRoutes = require('./routes/api');
 
 // Specify Port
 const PORT = process.env.PORT || 3001;
+
+// Initialize app
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.use(express.static('public'));
+
+// Listen for port
+app.listen(PORT, () => 
+    console.log(`App is listening at http://localhost:${PORT}`)
+);
+    
