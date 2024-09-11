b1f661dbc3d58a12fabb867af2b6a4b3132bab87
yinwu173 <yin.wu173@gmail.com>
2024-09-10 20:40:44 -0700

    Edited server.js: added pseudocode, imported express package, require the HTML and API file, then specified the port 3001

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+// Import express package
+const express = require('express');
+
+
 // HTML routes should be created
-
-
+// Require the HTML file and assign it to a variable called htmlRoutes
+const htmlRoutes = require('./routes/html');
 
 // API routes should be created
+// Require the API file and assign it to a variable called apiRoutes
+const apiRoutes = require('./routes/api');
+
+// Specify Port
+const PORT = process.env.PORT || 3001;
