3f38b2705d6c229c2cea06b2b836f511cc98f44d
yinwu173 <yin.wu173@gmail.com>
2024-09-15 12:07:41 -0700

    edited api.js: pushed new note to the notes array, then returned the note to the user

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,7 @@ const path = require('path');
 
 // Helper function to generate unique ids
 const uuid = require('../helpers/uuid');
+const { error } = require('console');
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 // Create GET route to read saved notes in json file
@@ -22,33 +23,37 @@ router.get('/notes', async (req, res) => {
 
 // POST /api/notes should receive a new note to save on the request body
 router.post('/notes', (req, res) => {
-    const notes = req.body;
+    const note = req.body;
     // Read the existing notes from db.json
     fs.readFile.readFile(path.join(__dirname, '..db/db.json'), 'utf8', (err, notes) => {
         if (err) {
             return res.status(500).json({ error: 'error' });
         }
-    // parse existing notes
-    notes = JSON.parse(notes);
-    
-    // if notes are present
-    if (notes) {
-        const newNote = {
-            title,
-            text,
-            id: uuid(),
-        };
+        // parse existing notes
+        note = JSON.parse(notes);
 
-        
-    }
+        // if notes are present
+        if (notes) {
+            // create a new note
+            const newNote = {
+                title,
+                text,
+                id: uuid(),
+            };
+            // add new note to the notes array
+            notes.push(newNote);
 
-    // Add it to the db.json file
-
-
-
-    // Then return the new note to the client. 
-    })
-})
+            // Add it to the db.json file and then return the new note to the client
+            fs.writeFile(path.join(__dirname, '..db/db.json'), JSON.stringify(newNotes), (err, newNote) => {
+                if (err) {
+                    return res.status(500).json({ error: 'Failed to save note' });
+                } else {
+                    res.status(201).json(newNote);
+                }
+            })
+        }
+    });
+});
 
 
 // Export apiRoutes
