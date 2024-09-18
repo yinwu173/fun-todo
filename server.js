f29430a0ba2106dc39d7391849eaf8a3b5fa89a7
yinwu173 <yin.wu173@gmail.com>
2024-09-17 20:06:42 -0700

    edited server.js: moved middleware before html and api routes

--- server.js
+++ server.js
@@ -19,10 +19,11 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-app.use(express.static('public'));
 
 // Listen for port
 app.listen(PORT, () => 
